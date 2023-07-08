934cca2ca9fc511ad4290077070766183b160b74
daGoodenough <dgoodenough@proton.me>
2023-07-08 15:32:47 -0700

    Add logo as banner

--- src/TopNav.js
+++ src/TopNav.js
@@ -4,8 +4,16 @@ import Navbar from 'react-bootstrap/Navbar';
 function TopNav() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary custom-navbar" data-bs-theme="dark" sticky="top">
-      <Container className='justify-content-center'>
-        <Navbar.Brand href="#home">Pledge For GenZ</Navbar.Brand>
+      <Container fluid className='justify-content-center'>
+        <Navbar.Brand href="#home" className='text-center'>
+        <img
+              src="/img/SVGLogo.svg"
+              width="60%"
+              // height="auto"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+        </Navbar.Brand>
       </Container>
     </Navbar>
   );
