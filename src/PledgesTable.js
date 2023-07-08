a4b6c488bc600867d2ce759434e9caab9c3d6d69
daGoodenough <dgoodenough@proton.me>
2023-07-08 15:33:26 -0700

    Show link to Twitter as fontawesome icon

--- src/PledgesTable.js
+++ src/PledgesTable.js
@@ -1,29 +1,42 @@
-import {Table} from 'react-bootstrap';
-const PledgesTable = ({entries}) => {
+import { Table } from 'react-bootstrap';
+const PledgesTable = ({ entries }) => {
   console.log(entries);
   return (
-   <Table striped variant='dark'>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Date Pledged</th>
-        <th>Amount (sats)</th>
-      </tr>
-    </thead>
-    <tbody>
-      {entries ? 
-        (entries.map((entry, index) => (
-          <tr key={index}>
-            <td><div>{entry.name}</div><div>{entry.twitter ? entry.twitter : null}</div></td>
-            <td>{entry.date}</td>
-            <td>{entry.amount_sats}</td>
-          </tr>
-        ))
-        ) :
-        "Loading"
-      }
-    </tbody>
-   </Table>
+    <Table striped variant='dark'>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Date Pledged</th>
+          <th>Amount (sats)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {entries ?
+          (entries.map((entry, index) => (
+            <tr key={index}>
+              <td>
+                <span>
+                  {entry.name + ' '}
+                </span> 
+                <span>
+                  {
+                    entry.twitter && (
+                      <a href={`https://twitter.com/${entry.twitter}`}>
+                        <i class="fa-brands fa-twitter"></i>
+                      </a>
+                    )
+                  }
+                </span>
+              </td>
+              <td>{entry.date}</td>
+              <td>{entry.amount_sats}</td>
+            </tr>
+          ))
+          ) :
+          "Loading"
+        }
+      </tbody>
+    </Table >
   );
 }
 
